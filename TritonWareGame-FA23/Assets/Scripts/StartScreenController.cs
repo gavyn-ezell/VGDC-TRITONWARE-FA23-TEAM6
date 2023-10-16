ca57a0679940d68a3600904322d155e4f8accb64
Gavyn Ezell <ezellgavyn@gmail.com>
2023-10-16 12:46:29 -0700

    start screen and transition

--- TritonWareGame-FA23/Assets/Scripts/StartScreenController.cs
+++ TritonWareGame-FA23/Assets/Scripts/StartScreenController.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class StartScreenController : MonoBehaviour
 {
-    // Update is called once per frame
+    public Animator fadeTransition;
+    public UnityEngine.UI.Image fadeTransitionPanel;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("BossFight", LoadSceneMode.Single);
+
+        fadeTransitionPanel.raycastTarget = true;
+        StartCoroutine(LoadLevel());
     }
 
     public void QuitGame()
     {
         Application.Quit();
 
+    }
+
+    IEnumerator LoadLevel()
+    {
+
+        fadeTransition.SetTrigger("CloseScene");
+        yield return new WaitForSeconds(4f);
+        SceneManager.LoadScene("BossFight");
     }
 }
