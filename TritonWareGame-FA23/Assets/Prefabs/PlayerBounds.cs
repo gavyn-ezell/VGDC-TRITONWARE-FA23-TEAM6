a1ee3fe4aee568dbcc74239987af61de7e9080eb
kaylamochizuki <mochizukikayla@gmail.com>
2023-10-19 21:33:31 -0700

    fixed bounds

--- TritonWareGame-FA23/Assets/Prefabs/PlayerBounds.cs
+++ TritonWareGame-FA23/Assets/Prefabs/PlayerBounds.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerBounds : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector2 bounds;
+    private Vector2 screenBounds;
+    private float objWidth;
+    private float objHeight;
     void Start()
     {
-        bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        objWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, bounds.x * -1, bounds.x);
-        pos.y = Mathf.Clamp(pos.y, bounds.y * -1, bounds.y);
+        pos.x = Mathf.Clamp(pos.x, screenBounds.x * -1 + objWidth, screenBounds.x - objWidth);
+        pos.y = Mathf.Clamp(pos.y, screenBounds.y * -1 + objHeight, screenBounds.y - objHeight);
         transform.position = pos;
     }
 }
